f8304a7a0e92aa731cfc0cdc44dde68c2adcda1e
Neil Dobson <ndobson@barnardos.org.au>
2020-01-20 11:58:53 +1100

    Remove comments. Doesn't really help

--- src/NUnit.Examples.Impl/PayCalcs.cs
+++ src/NUnit.Examples.Impl/PayCalcs.cs
@@ -11,31 +11,23 @@ namespace NUnit.Examples.Impl
             DateTime reportStartDate,
             DateTime reportEndDate)
         {
-            // startDate = Latest of either workPeriodStartDate or reportStartDate
             var startDate = workPeriodStartDate >= reportStartDate
                 ? workPeriodStartDate
                 : reportStartDate;
 
-            // endDate = workPeriodEndDate unless null, otherwise reportEndDate
             var endDate = workPeriodEndDate ?? reportEndDate;
-            // If endDate > reportEndDate then set to reportEndDate
             endDate = endDate <= reportEndDate ? endDate : reportEndDate;
 
-            // if workPeriodEndDate is null or > reportEndDate
-            // then return end - start + 1 
             if (workPeriodEndDate == null || workPeriodEndDate > reportEndDate)
             {
                 return endDate.Subtract(startDate.Date).Days + 1;
             }
 
-            // If workPeriodStartDate == workPeriodEndDate return 1
             if (workPeriodStartDate == workPeriodEndDate.Value)
                 return 1;
 
-            // days = workPeriodEndDate - startDate
             var days = workPeriodEndDate.Value.Subtract(startDate.Date).Days;
             
-            // Return days + 1 unless daysNightsType = Nights, then return days
             return daysNightsType == DaysNightsType.Nights ? days : days + 1;
         }
     }
